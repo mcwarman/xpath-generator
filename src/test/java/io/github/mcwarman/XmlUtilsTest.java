ebe14aa569fe1fb8fda1852da7480633a4e13042
mcwarman <mcwarman@gmail.com>
2018-09-17 17:08:15 +0100

    Clean up resources

--- src/test/java/io/github/mcwarman/XmlUtilsTest.java
+++ src/test/java/io/github/mcwarman/XmlUtilsTest.java
@@ -13,7 +13,7 @@ public class XmlUtilsTest {
 
   @Test
   public void resolveXincludes() throws Exception {
-    File file = new File(this.getClass().getClassLoader().getResource("adapter.xml").getFile());
+    File file = new File(this.getClass().getClassLoader().getResource("test.xml").getFile());
     String xml = "<xi:include xmlns:xi=\"http://www.w3.org/2001/XInclude\" href=\"file:///" + file.getAbsolutePath()  +"\"/>";
     String result = XmlUtils.resolveXincludes(xml);
     assertFalse(result.contains("xi:include"));
